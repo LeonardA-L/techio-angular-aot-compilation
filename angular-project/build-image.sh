2006ac855da1fdec099a1b051778f031384b1896
Leonard Allain-Launay <leonard@codingame.com>
2017-06-14 16:06:31 -0700

    Add course content on angular AoT

--- angular-project/build-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /project/target
-
-npm install --max-old-space-size=750
--- angular-project/src/main.ts
+++ angular-project/src/main.ts
@@ -1,4 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+console.log('??', (new Date()).getTime())
 platformBrowserDynamic().bootstrapModule(AppModule);
+console.log('!', (new Date()).getTime())
